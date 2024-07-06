ed82ed206303916274d1e14915ca66b0b2f50ccc
Parth Antala <52197140+parallel-appy@users.noreply.github.com>
2024-07-05 20:19:14 -0400

    Initial Commit

---
+++ Units onTheGo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Units onTheGo
+//
+//  Created by Parth Antala on 2024-07-05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Units onTheGo/Units_onTheGoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Units_onTheGoApp.swift
+//  Units onTheGo
+//
+//  Created by Parth Antala on 2024-07-05.
+//
+
+import SwiftUI
+
+@main
+struct Units_onTheGoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
